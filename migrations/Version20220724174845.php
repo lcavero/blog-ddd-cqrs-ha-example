14b35e99489e490f6b3cd6b4e030973c1c88d131
luiscavero <luiscaverodeveloper@gmail.com>
2022-07-24 19:51:18 +0200

    Doctrine mappings and migrations

---
+++ migrations/Version20220724174845.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220724174845 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE post (id VARCHAR(255) NOT NULL, author_id VARCHAR(255) DEFAULT NULL, title VARCHAR(255) NOT NULL, body VARCHAR(1000) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_5A8A6C8DF675F31B ON post (author_id)');
+        $this->addSql('CREATE TABLE "user" (id VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DF675F31B FOREIGN KEY (author_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE post DROP CONSTRAINT FK_5A8A6C8DF675F31B');
+        $this->addSql('DROP TABLE post');
+        $this->addSql('DROP TABLE "user"');
+    }
+}
